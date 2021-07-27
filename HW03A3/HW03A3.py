50894b0537fe1bd6b23137b12c82654fbe09f62c
SonramSirirat <s.sonram@gmail.com>
2021-07-27 19:35:09 +0700

    Add bot python

--- HW03A3/HW03A3.py
+++ HW03A3/HW03A3.py
@@ -12,8 +12,8 @@ Experience: Game pattern that Python can win the game
 
 # Ask number of sticks in pile
 stick = int(input("How many sticks in the pile🍡: "))
-losing_position = stick - 1
 print("There are", stick, "sticks in the pile.🍡")
+
 
 # Ask player's name
 # player_name = str(input("What is your name😃: "))
@@ -22,31 +22,42 @@ print("There are", stick, "sticks in the pile.🍡")
 #     player_take = int(input(" how many stick you will take (1 or 2): "))
 #     return player_take
 
+losing_position = stick - 1
+print("Losing position is", losing_position)
 # Stick in the pile
 while stick > 0:
+    # 7 > 0
     player_take = int(input("How many stick you will take (1 or 2): "))
-    # stick = stick - player_take
+    # 2
+    stick = stick - player_take
+    # 5 = 7 - 2
     print("Player take", player_take, "stick(s)")
     if stick == 0:
         print("No stick left")
     else:
-        # print(stick, "stick(s) left")
-        # If player take 1 stick
-        if player_take == 1:
-            stick = stick - player_take
-            print(stick, "stick(s) left")
-            # Python do
-            if stick > 0:
-                stick = stick - 2
+        print(stick, "stick(s) left")
+        # target acquired ready to engage!
+        if stick == losing_position:        
+            if stick == 2:
+                stick = stick - 1
+                print("Python take 1 stick")
+                print(stick, "stick(s) left")
+            else:
+                stick = losing_position - 2
                 print("Python take 2 sticks")
                 print(stick, "stick(s) left")
-        
-        # If player take 2 sticks      
-        if player_take == 2:
-            stick = stick - player_take
-            print(stick, "stick(s) left")
-            # Python do
-            if stick > 0:
+        elif stick != losing_position:
+            # 5 != 6
+            if stick == 2:
                 stick = stick - 1
+                print("Python take 1 stick")
+                print(stick, "stick(s) left")
+            elif stick == 1:
+                stick = stick - 1
+                print("Python take the last stick you win!")
+            elif stick < losing_position:
+                # 5 < 6
+                stick = stick - 1
+                # 4 = 5 - 1
                 print("Python take 1 stick")
                 print(stick, "stick(s) left")
