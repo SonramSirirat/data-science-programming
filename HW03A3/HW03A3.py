72668b2773f2e0f976b0db5162b2675bdac8edba
SonramSirirat <s.sonram@gmail.com>
2021-07-27 18:47:07 +0700

    Add losing postion = stick - 1

--- HW03A3/HW03A3.py
+++ HW03A3/HW03A3.py
@@ -12,6 +12,7 @@ Experience: Game pattern that Python can win the game
 
 # Ask number of sticks in pile
 stick = int(input("How many sticks in the pile🍡: "))
+losing_position = stick - 1
 print("There are", stick, "sticks in the pile.🍡")
 
 # Ask player's name
@@ -23,14 +24,29 @@ print("There are", stick, "sticks in the pile.🍡")
 
 # Stick in the pile
 while stick > 0:
-    player_take = int(input(" how many stick you will take (1 or 2): "))
-    stick = stick - player_take
+    player_take = int(input("How many stick you will take (1 or 2): "))
+    # stick = stick - player_take
     print("Player take", player_take, "stick(s)")
     if stick == 0:
         print("No stick left")
     else:
-        print(stick, "stick(s) left")
-        # Python AI
-        # if stick
+        # print(stick, "stick(s) left")
+        # If player take 1 stick
+        if player_take == 1:
+            stick = stick - player_take
+            print(stick, "stick(s) left")
+            # Python do
+            if stick > 0:
+                stick = stick - 2
+                print("Python take 2 sticks")
+                print(stick, "stick(s) left")
         
-        
+        # If player take 2 sticks      
+        if player_take == 2:
+            stick = stick - player_take
+            print(stick, "stick(s) left")
+            # Python do
+            if stick > 0:
+                stick = stick - 1
+                print("Python take 1 stick")
+                print(stick, "stick(s) left")
