b8837333810701d6d4438cf537c951f0125b9df1
SonramSirirat <s.sonram@gmail.com>
2021-07-30 23:21:43 +0700

    Try to convert input to output values

--- HW04B1/HW04B1_640631037.py
+++ HW04B1/HW04B1_640631037.py
@@ -10,15 +10,27 @@ Created on Wed Jul 28 15:30:08 2021
 # 
 # For example (50,2,1,9), the largest formed number is 95021
 # 
-# Be carefull if [10,1] the answer is 110 not 101 
+# Be carefull if [10,1] the answer is 110 not 101
 # =============================================================================
-user_input = [int(x) for x in input('User Input: ').split()]
-
-i = 0
-print('Key', 'Value')
-while i < len(user_input):
-    # 0 < 4, 0 < 3, 0 < 2, 0 < 1
-    print(i, user_input[i])
-    # if user_input[i] >:
-    #     # [0]50 > 
-    i += 1
+# num_list = [50, 2, 1, 9]
+# n = str(num_list[0])
+# print(type(n))
+    
+# user_input = [int(x) for x in input('User Input: ').split()]
+user_input = [50, 2, 1, 9]
+for x in range(len(user_input)):
+    # print(user_input[x])
+    print(user_input)
+    
+    
+# user_input2 = str(user_input[1])
+# user_input3 = str(user_input[2])
+# user_input4 = str(user_input[3])
+# output = user_input1 + user_input2 + user_input3 + user_input4
+# print(output)
+# i = 0
+# print('Key', 'Value')
+# while i < len(user_input):
+#     print(i, user_input[i])
+#     i += 1
+# print(user_input.sort(10))
