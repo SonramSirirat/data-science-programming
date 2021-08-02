f9087561f1349ff2a1ff7968636ecb1c8b1a8ee5
SonramSirirat <s.sonram@gmail.com>
2021-08-02 18:46:34 +0700

    Fishish HW04B1

--- HW04B1/HW04B1_640631037.py
+++ HW04B1/HW04B1_640631037.py
@@ -16,12 +16,32 @@ Created on Wed Jul 28 15:30:08 2021
 # Get user input
 # user_input = list(input("Type here: "))
 user_input = [50, 2, 1, 9]
+# user_input = [10, 1]
 print('List of positive integers:', user_input)
 
-# Find the greater integer in the list
-greater_int = max(user_input)
-print('Greater number of digits:', greater_int)
+# Find the largest integer in the list
+largest_int = max(user_input)
+print('Largest integer:', largest_int)
 
-# Count digits of greater interger in the list
-digits_count = len(str(greater_int))
-print('Leght number of greater number:', digits_count)
+# Count lenght of largest integer
+larg_len_int = len(str(largest_int))
+print('Lenght of largest integer:', larg_len_int)
+
+# Create a dictionary contain a integer to compare each others
+dict_int = {}
+for i in user_input:
+    length_int = len(str(i))
+    
+    if length_int == 1:
+        dict_int[i] = (i + 1) * 10 ** (larg_len_int - length_int) - 1
+    else:
+        dict_int[i] = i * 10 ** (larg_len_int - length_int)
+print('Dictionary:', dict_int)
+
+# Sort dictionary value and rearrage new integers
+sorted_dict_int = sorted(dict_int, key=dict_int.__getitem__, reverse=True)
+print('Rearrage number:', sorted_dict_int)
+
+# Convert dictionary to string
+output = ''.join(str(o) for o in sorted_dict_int)
+print('The largest formed number is', output)
