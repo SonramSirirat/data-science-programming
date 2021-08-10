e58b12e7e8f89c71362cc9f4ea856975adc98554
SonramSirirat <s.sonram@gmail.com>
2021-08-10 11:44:11 +0700

    Done homework.

--- HW05/HW05_640631037.py
+++ HW05/HW05_640631037.py
@@ -1,14 +1,69 @@
-vanilla_ing = 0.5
-print('Vanilla ingredients', vanilla_ing)
-
-strawberry_ing = 0.2
-print('Strawberry ingredients', strawberry_ing)
-
 fresh_milk = 10
-print('Fresh milk per day', fresh_milk)
+dolls = 30
+vanilla_profit = 2
+strawberry_profit = 3
+vanilla_use = 0.5 # Vanilla ice cream use 0.5 liter fresh milk
+strawberry_use = 0.2 # Strawberry ice cream use 0.2 liter fresh milk
+min_fresh_milk_use = min([vanilla_use, strawberry_use]) # Find minumum fresh milk use
 
-x = fresh_milk / vanilla_ing
-print('Amount of vanilla ice cream can make with others favor', x, 'Profit', x * 2)
+if fresh_milk >= min_fresh_milk_use:
+    
+    if strawberry_profit > vanilla_profit:
+        
+        if strawberry_use <= vanilla_use:
+            
+            # Make Strawberry ice cream
+            strawberry_ice = int(fresh_milk / strawberry_use)
+            print('Make', strawberry_ice, 'Strawberry ice cream boxes')
+            
+            fresh_milk = fresh_milk - (strawberry_ice * strawberry_use)
+            print('Fresh Milk:', fresh_milk, 'lites remaining')
+            # Dolls remaining
+            if strawberry_ice >= dolls:
+                promotion = 1 * dolls
+                print('Promotion:', promotion, 'Dolls')
+                dolls = dolls - dolls
+                print('Dolls:', dolls, 'remaining')
+            else:
+                promotion = 1 * strawberry_ice
+                print('Promotion:', promotion, 'Dolls')
+                dolls
+            
+            # Strawberry Profit
+            strawberry_ice_profit = strawberry_ice * strawberry_profit
+            print('Profit($):', strawberry_ice_profit)
+    
+    else:
+        # Make Vanilla ice cream
+        vanilla_ice = int(fresh_milk / vanilla_use)
+        print('Make', vanilla_ice, 'Vanilla ice cream boxes')
+         
+        # Fresh milk remaining
+        fresh_milk = fresh_milk - (vanilla_ice * vanilla_use)
+        print('Fresh Milk:', fresh_milk, 'lites remaining')
+        
+        if fresh_milk > min_fresh_milk_use:
+            # Make Strawberry ice cream
+            strawberry_ice = int(fresh_milk / strawberry_use)
+            print('Make', strawberry_ice, 'Strawberry ice cream boxes')
+        
+        ice_cream_make = vanilla_ice + strawberry_ice
+        # print(ice_cream_make)
+        
+        # Dolls remaining
+        if ice_cream_make >= dolls:
+            promotion = 1 * dolls
+            print('Promotion:', promotion, 'Dolls')
+            dolls = dolls - dolls
+            print('Dolls:', dolls, 'remaining')
+        else:
+            promotion = 1 * ice_cream_make
+            print('Promotion:', promotion, 'Dolls')
+            dolls
+        
+        # Profit
+        ice_cream_profit = (vanilla_ice * vanilla_profit) + (strawberry_ice * strawberry_profit)
+        print('Profit($):', ice_cream_profit)
 
-y = fresh_milk / strawberry_ing
-print('Amount of strawberry ice cream can make with others favor', y, 'Profit', x * 3)
+else:
+    print('Fresh milk not enough')
--- HW05/README.md
+++ HW05/README.md
@@ -2,11 +2,48 @@
 
 Assume you open your ice cream shop, there are only two types of ice cream, vanilla and strawberry. When a box of ice cream is sold, you will get the benefit for $ 2 for vanilla ice cream and $ 3 for strawberry ice cream. To make the ice cream, the fresh milk is required. To make a box of vanilla ice cream requires 0.5 liter and strawberry ice cream requires 0.2 liter. You daily order 10 liters of fresh milk. To promote your ice cream shop, you give a doll for each ice cream box. The number of dolls, that you can give to customers, is 30 dolls per day. So, how many boxes of vanilla ice cream and strawberry ice cream that you would like to produce to get maximum profit ?
 
-| Favor      | Profit $/(Box) | Fresh milk/(Liter) |
-| ---        | ---            | ---                |
-| Vanilla    | 2              | 0.5                |
-| Strawberry | 3              | 0.2                |
+| Favor      | Fresh milk/(Liter) | Profit $/(Box) |
+| ---        | ---                | ---            |
+| Vanilla    | 0.5                | 2              |
+| Strawberry | 0.2                | 3              |
 
-**Constrain**
 - Order daily 10 liters of fresh milk
 - Dolls for each ice cream box. (30 per day)
+
+**Identify Problem**
+
+How many boxes of vanilla ice cream and strawberry ice cream that you would like to produce to get **maximum profit**
+
+**Formulate and Implement Model**
+
+- Decision variables
+  
+  vanilla_ice: The amount of vanilla ice cream boxes.
+
+  strawberry_ice: The amount of strawberry ice cream boxes.
+
+- Ojective function
+  
+  Find profit of ice cream
+
+  vanilla_profit = 2 * vanilla_ice: Equal profit of vanilla ice cream.
+
+  strawberry_profit = 3 * strawberry_ice: Equal profit of strawberry ice cream.
+
+  maximun_profit = vanilla_profit + strawberry_profit: Maximum profit to produce.
+
+- Constraints
+  - Vanilla ice cream use fresh milk for 0.5 per boxes.
+    - 0.5 * vanilla_ice <= 10: Maximum vanilla ice cream boxes can make per day.
+  - Strawberry ice cream use fresh milk for 0.2 per boxes.
+    - 0.2 * strawberry_ice <= 10: Maximum strawberry ice cream boxes can make per day.
+  - Maximum fresh milk is 10 liters per day.
+    - (0.5) * vanilla_ice + (0.2) * strawberry_ice <= 10: Maximum both favor ice cream boxes can make per day.
+  - Maximum doll is 30 dolls per day 1 doll per boxes.
+    - (0.5) * vanilla_ice + (0.2) * strawberry_ice <= 30: Maximun dolls for promotion per day.
+
+**Analyze Model**
+
+**Test Results**
+
+**Implement Solution**
